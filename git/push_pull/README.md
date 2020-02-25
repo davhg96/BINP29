5dce90536cd61cfc1b1fb28b799e965d3e77cbfb
BINP (2019) 40 <bio1940@binp15.mbfys.lu.se>
2020-02-25 11:34:48 +0100

    first commit

---
+++ git/push_pull/README.md
@@ -0,0 +1,2 @@
+A Hello world collection
+Hello world in differnt languages
---
+++ git/push_pull/python_hi.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import random
+
+print("Hello world")
+print("Hello world from master")
+
+if random.random() > 0.5:
+	print("Again; Hello world!!")
